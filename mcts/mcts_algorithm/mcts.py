9253e30b71e3b8fb7169ffad402b4401c4aa9df8
Nikita Kotelevskii <nikita.kotelevskii@skoltech.ru>
2021-05-27 14:08:35 +0600

    Minors: reordered cumsum and added depth print

--- mcts/mcts_algorithm/mcts.py
+++ mcts/mcts_algorithm/mcts.py
@@ -27,7 +27,10 @@ class Tree:
 
     def update_tree(self, env, node_id, outcome):
         cum_reward = outcome
+        t = 0
         while node_id != -2:
+            cum_reward += env.reward(self.states[node_id].e_state, self.states[node_id].p_state)
             self.states[node_id].update_value(outcome=cum_reward)
             node_id = self.states[node_id].parent_id
-            cum_reward += env.reward(self.states[node_id].e_state, self.states[node_id].p_state)
+            t += 1
+        print("Tree depth: ", t)
